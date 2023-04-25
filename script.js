fec01d5779fa25786a508ab2ba7420ab303b1438
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-25 14:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+      var line = document.getElementById('line');
+      var angle = 0;
+      setInterval(function() {
+        angle += 1;
+        line.style.transform = 'rotate(' + angle + 'deg)';
+      }, 10);
